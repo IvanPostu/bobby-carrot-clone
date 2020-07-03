8e49317f448c59fdb478962b42eecf67fb2bf14a
IvanPostu <ipostu20000127@gmail.com>
2020-07-03 22:15:06 +0300

    Project created, added maven, pom.xml is configured.

---
+++ src/test/java/com/utm/MainTest.java
@@ -0,0 +1,23 @@
+package com.utm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest 
+{
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue()
+  {
+    int n = 1;
+    n += 10;
+    n += 22;
+    assertTrue( n == 33 );
+  }
+}
